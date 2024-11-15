138cde840a4b9bbec2ccc5462f73ff20bce08daf
Schwartz <dev@bizuu21.com.br>
2024-11-15 20:08:48 -0300

    Chore: removed auto call chat on start

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import "express-async-errors";
 import cors from 'cors';
 import ErrorCatcher from "@/middlewares/errors.middleware";
 import MainRouter from "./routes/index.routes";
+import OpenAI from "openai";
+const openai = new OpenAI();
 
 const app = express();
 
@@ -12,6 +14,22 @@ app.use(MainRouter);
 // app.use(ErrorCatcher);
 const PORT = process.env.PORT || 5000;
 
+async function startChatGPT() {
+  // const completion = await openai.chat.completions.create({
+  //   model: "gpt-3.5-turbo",
+  //   messages: [
+  //     { role: "system", content: "You are a helpful assistant." },
+  //     {
+  //       role: "user",
+  //       content: "How many stars are in the Milky Way?",
+  //     },
+  //   ],
+  // });
+  // console.log(JSON.stringify(completion, null, 2));
+}
+
+
 app.listen(PORT, () => {
   console.log(`--------------- Server is up and running on port ${PORT}`);
+  startChatGPT();
 });
