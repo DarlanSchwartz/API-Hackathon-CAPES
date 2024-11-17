40a340ff28d45e38bb0e24cd04d3024d58763dff
Schwartz <dev@bizuu21.com.br>
2024-11-17 09:17:11 -0300

    Feat: added health check route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,4 +3,7 @@ import ChatRouter from "./chat.routes";
 
 const MainRouter = Router();
 MainRouter.use(ChatRouter);
+MainRouter.get("/health", (req, res) => {
+    res.status(200).send("Server is up and running");
+});
 export default MainRouter;
